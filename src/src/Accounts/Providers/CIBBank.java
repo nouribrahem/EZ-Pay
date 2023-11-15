fb2669e5cd2a09d92b5e581f099886f6712d178e
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-11-15 13:52:17 +0200

    cib bank

--- src/src/Accounts/Providers/CIBBank.java
+++ src/src/Accounts/Providers/CIBBank.java
@@ -1,12 +1,8 @@
 package Accounts.Providers;
 
-import Accounts.Account;
-import Accounts.BankAccount;
 
-import java.util.Random;
 
 public class CIBBank extends BankAccountProvider {
-    @Override
     public void fillAccounts() {
         super.fillAccounts();
     }
